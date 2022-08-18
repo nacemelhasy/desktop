c83f8cde3e265a4407452ceed9b4e5f549dc1638
nacemelhasy <nacem09@gmail.com>
2022-08-18 06:11:40 +0200

    First pushed

---
+++ lib/Home.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class Home extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+ return Directionality(
+  textDirection: TextDirection.rtl,
+   child: Scaffold(
+ 
+    body: Stack(
+      textDirection:TextDirection.rtl ,
+      children: [
+        Positioned(
+          right: 0,
+          child: Container(
+            height: MediaQuery.of(context).size.height,
+            width: MediaQuery.of(context).size.height*0.3,
+            decoration:  BoxDecoration(
+              color:const Color.fromARGB(255, 255, 8, 8),
+        border: Border.all(
+          color: Colors.white,
+          width: 4
+        ),
+        boxShadow:const [
+           BoxShadow(
+            color: Color.fromARGB(85, 0, 0, 0),
+            blurRadius: 4,
+            offset: Offset(-6, 4)
+            
+          )
+        ],
+              borderRadius:const BorderRadius.only(
+                topLeft: Radius.circular(25),
+                bottomLeft: Radius.circular(25),
+              )
+            ),
+          ),
+        )
+      ],
+    ),
+   ),
+ );
+  }
+
+}
