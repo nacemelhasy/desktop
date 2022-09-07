e54e518404edd9852909b6561784e45b0af0bd29
nacemelhasy <nacem09@gmail.com>
2022-09-07 05:47:53 +0200

    at last chnges for push on google play store

--- lib/Home.dart
+++ lib/Home.dart
@@ -1,3 +1,5 @@
+import 'package:desktop/compenets/allCompontes.dart';
+import 'package:desktop/compenets/defaultCS.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -8,36 +10,67 @@ class Home extends StatelessWidget{
   textDirection: TextDirection.rtl,
    child: Scaffold(
  
-    body: Stack(
-      textDirection:TextDirection.rtl ,
-      children: [
-        Positioned(
-          right: 0,
-          child: Container(
+    body: Container(
+      color: Colors.white,
+      height: height(context),
+      width: width(context),
+      child: Row(
+        textDirection:TextDirection.rtl ,
+        children: [
+           
+          Container(
             height: MediaQuery.of(context).size.height,
-            width: MediaQuery.of(context).size.height*0.3,
+            width: MediaQuery.of(context).size.height*0.4,
             decoration:  BoxDecoration(
               color:const Color.fromARGB(255, 255, 8, 8),
-        border: Border.all(
+          border: Border.all(
           color: Colors.white,
           width: 4
-        ),
-        boxShadow:const [
+          ),
+          boxShadow:const [
            BoxShadow(
             color: Color.fromARGB(85, 0, 0, 0),
             blurRadius: 4,
             offset: Offset(-6, 4)
             
           )
-        ],
+          ],
               borderRadius:const BorderRadius.only(
-                topLeft: Radius.circular(25),
-                bottomLeft: Radius.circular(25),
+                topLeft: Radius.circular(10),
+                bottomLeft: Radius.circular(10),
               )
             ),
           ),
-        )
+          SizedBox(
+            width: width(context)*0.15,
+          ),
+    Column(
+      children: [
+
+          Align(
+                alignment: Alignment.topCenter,
+                child: SizedBox(
+                      height: height(context)*0.1,
+                      width: width(context)*0.5,
+                      child: 
+                    fitted( text(context, 'منظومة الباش مهندسين')),
+                    ),
+              ),
+                  Align(
+                alignment: Alignment.topCenter,
+                child: SizedBox(
+                      height: height(context)*0.1,
+                      width: width(context)*0.5,
+                      child: 
+                    fitted( text(context, 'Engineers system')),
+                    ),
+              ),
       ],
+    )
+
+     
+        ],
+      ),
     ),
    ),
  );
